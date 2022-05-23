835091b0e3b4b67bb414d1d696c675bc9a48b0c6
Spencer Mathews <smathews0906@gmail.com>
2022-05-23 10:07:56 -0500

    Add css, js,and HTML

--- script.js
+++ script.js
@@ -3,3 +3,12 @@ function contact(){
     document.getElementById("clickhere").innerHTML = "Spencer Mathews<br>Email: Smathews0906@gmail.com<br>Cell: 816-572-1920";
     
 }
+
+function myFunction() {
+    var x = document.getElementById("navlist");
+    if (x.className === "nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "nav";
+    }
+  }
